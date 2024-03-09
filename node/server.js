78f47b1a57fb1ff4a6a84574ea2b17d88b9f22e3
Vinicius <vinicius.padua@tarvos.ag>
2024-03-09 18:37:35 -0300

    first commit

---
+++ node/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+const faker = require('faker')
+
+const app = express();
+const port = 5000;
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+};
+const connection = mysql.createConnection(config);
+
+
+app.get('/', (req, res) => {
+  const name = faker.name.findName()
+
+  connection.query(`INSERT INTO people (nome) VALUES ('${name}')`);
+
+  connection.query(`SELECT nome FROM people`, (error, results, fields) => {
+
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `);
+  });
+});
+
+app.listen(port, () => console.log(`Server is up and running at port ${port}`));
